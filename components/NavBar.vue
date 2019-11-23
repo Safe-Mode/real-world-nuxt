97b4f3cceebf222f69e9748207537f64eabfa446
Safe_Mode <safe2mode@gmail.com>
2019-11-23 14:34:31 +0300

    Create nested route, dynamic route, custom error page

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="nav">
-    <nuxt-link to="/" class="brand">Real World Eevnts</nuxt-link>
+    <nuxt-link to="/" class="brand">Real World Events</nuxt-link>
 
     <nav>
       <nuxt-link to="/">List</nuxt-link>&nbsp;|
-      <nuxt-link to="/create">Create</nuxt-link>
+      <nuxt-link to="/event/create">Create</nuxt-link>
     </nav>
   </div>
 </template>
